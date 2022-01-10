4ba5f7e299df6f5c284c9f815b23577c8734b0b4
IsraelAC <israel.aguilarc@promoespacio.com.mx>
2022-01-10 07:24:28 -0600

    avance pantalla menu

--- Proyecto/inventario_urba/lib/screens/menu.dart
+++ Proyecto/inventario_urba/lib/screens/menu.dart
@@ -29,10 +29,24 @@ class _Menu extends StatelessWidget {
             color: AppTheme.primary,
             width: MediaQuery.of(context).size.width * 0.15,
             height: double.infinity,
+            child: SafeArea(
+              child: Column(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.close,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
           Container(
-            margin:
-                EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.15),
+            margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.15),
             width: MediaQuery.of(context).size.width * 0.85,
             height: double.infinity,
             child: SafeArea(
@@ -41,20 +55,42 @@ class _Menu extends StatelessWidget {
               children: [
                 const Image(image: AssetImage("assets/login.png")),
                 const Divider(),
-                _paddignText("Usuario Restringido", 18, FontWeight.bold, 25,
-                    AppTheme.primary_shade(40)),
                 _paddignText(
-                    "inventarioapp@gmail.com", 17, FontWeight.normal, 10),
-                _paddignText("Entidades", 20, FontWeight.bold, 20,
-                    AppTheme.primary_shade(40)),
+                    "Usuario Restringido", 18, FontWeight.bold, 25, 25, AppTheme.primary_shade(40)),
+                _paddignText("inventarioapp@gmail.com", 17, FontWeight.normal, 10, 25),
+                _paddignText("Entidades", 20, FontWeight.bold, 20, 25, AppTheme.primary_shade(40)),
                 _paintPermisions(['sucursales', 'inventario']),
                 const Divider(),
-                _paddignText("Cambiar Contraseña", 20, FontWeight.bold, 20,
-                    AppTheme.primary_shade(40)),
+                _paddingAcount(),
+                _paddignText(
+                    "Cambiar Contraseña", 18, FontWeight.w500, 20, 45, AppTheme.primary_shade(40)),
+                _addExit()
               ],
             )),
           ),
         ],
+      ),
+    );
+  }
+
+  Padding _paddingAcount() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+                leading: Icon(
+                  Icons.person,
+                  color: AppTheme.primary_shade(40),
+                ),
+                title: Text(
+                  'Cuenta',
+                  style: TextStyle(color: AppTheme.primary_shade(40)),
+                ),
+                onTap: () {}),
+          ],
+        ),
       ),
     );
   }
@@ -135,11 +171,36 @@ class _Menu extends StatelessWidget {
     );
   }
 
+  Padding _addExit() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25, top: 25),
+      child: Row(
+        children: [
+          Icon(
+            Icons.logout,
+            color: AppTheme.primary_shade(50),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 25),
+            child: Text(
+              'Cerrar Sesión',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: AppTheme.primary_shade(50),
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Padding _paddignText(
-      String texto, double size, FontWeight weight, double topSpace,
+      String texto, double size, FontWeight weight, double topSpace, double leftSpace,
       [Color colorText = Colors.black87]) {
     return Padding(
-      padding: EdgeInsets.only(left: 25.0, top: topSpace),
+      padding: EdgeInsets.only(left: leftSpace, top: topSpace),
       child: Text(texto,
           textAlign: TextAlign.left,
           textWidthBasis: TextWidthBasis.parent,
